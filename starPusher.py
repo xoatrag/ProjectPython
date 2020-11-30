f3fa10d4693ca0cfad452a817d0465e09f5feb1a
xoatrag <hoangtrungkien0901@gmail.com>
2020-12-01 00:01:19 +0700

    width, height of the window, in pixels

--- starPusher.py
+++ starPusher.py
@@ -3,4 +3,9 @@ Created on Nov 30, 2020
 
 @author: Admin
 '''
-print("hello")
+import random, sys, copy, os, pygame
+from pygame.locals import *
+
+FPS = 30 # frames per second to update the screen
+WINWIDTH = 800 # width of the window, in pixels
+WINHEIGHT = 600 # height in pixels
